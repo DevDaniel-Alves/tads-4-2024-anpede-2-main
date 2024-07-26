d9ac75a89ac65803f339352bcf03837a01c323aa
DANIEL GABRIEL ALVES <07556510140@ifms.edu.br>
2024-07-25 22:27:43 -0400

    codigo atualizado

--- src/main/java/com/anpede/TadsAnpedeApplication.java
+++ src/main/java/com/anpede/TadsAnpedeApplication.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
-
 public class TadsAnpedeApplication implements WebMvcConfigurer {
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TadsAnpedeApplication.class, args);
 	}
